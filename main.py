e4136435a5eb471aea3ae0d200272909f5fb02f6
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-07-24 02:06:05 +0900

    クエリとパスのパラメーターの組み合わせ

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ from fastapi import FastAPI
 app = FastAPI()#インスタンス化
 
 # クエリパラメーター
-@app.get("/countries/")#ルーティング　@はdecorator
-async def country(country_name: str = 'japan',country_no: int =1):#パスパラメーターはないけど、引数が設定されているのがクエリパラメーター
+@app.get("/countries/{country_name}")#ルーティング　@はdecorator
+async def country(country_name: str = 'japan',city_name: str = 'Tokyo'):#パスパラメーターはないけど、引数が設定されているのがクエリパラメーター
     return {
         "country_name": country_name,
-        "country_no": country_no
+        "city_name": city_name
     }
 
 #http://127.0.0.1:8000/countries/?country_name=England&country_no=3
+
+#http://127.0.0.1:8000/countries/england?city_name=London
 
 #パスパラメーター
 # @app.get("/countries/japan")#ルーティング　@はdecorator
