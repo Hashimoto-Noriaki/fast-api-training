23e0207c80edd10b9119deb2474cab4aa3d171fe
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-07-24 02:17:12 +0900

    必須ではないパラメーターの設定

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()#インスタンス化
 
-# クエリパラメーター
+#必須でないパラメーター
 @app.get("/countries/")#ルーティング　@はdecorator
-async def country(country_name: str = 'japan',country_no: int =1):#パスパラメーターはないけど、引数が設定されているのがクエリパラメーター
+async def country(country_name: Optional[str] = None, country_no: Optional[int] = None):
     return {
         "country_name": country_name,
         "country_no": country_no
     }
+
+#http://127.0.0.1:8000/countries/
+
+# クエリパラメーター
+# @app.get("/countries/")#ルーティング　@はdecorator
+# async def country(country_name: str = 'japan',country_no: int =1):#パスパラメーターはないけど、引数が設定されているのがクエリパラメーター
+#     return {
+#         "country_name": country_name,
+#         "country_no": country_no
+#     }
 
 #http://127.0.0.1:8000/countries/?country_name=England&country_no=3
 
